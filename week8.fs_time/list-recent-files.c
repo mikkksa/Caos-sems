32520ada09ad462d279cdc65b11dfdd7c820218a
Mikhail Molchanov <m.mol4anov2010@yandex.ru>
2020-10-21 16:41:40 +0300

    week8 added

---
+++ week8.fs_time/list-recent-files.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    const char * dir_path = argv[1];
+    int year = strtol(argv[2], NULL, 10);
+    int mon = strtol(argv[3], NULL, 10);
+    int day = strtol(argv[4], NULL, 10);
+    int dirfd = open(dir_path, O_RDONLY | O_DIRECTORY);
+    DIR *dir = fdopendir(dirfd);
+    struct dirent *entry;
+    struct stat st;
+    struct tm max_tm;
+    memset(&max_tm, 0, sizeof(max_tm));
+    max_tm.tm_year = year - 1900;
+    max_tm.tm_mon = mon - 1;
+    max_tm.tm_mday = day;
+    max_tm.tm_isdst = -1;
+    time_t max_time = mktime(&max_tm);
+    while ((entry=readdir(dir))) {
+        fstatat(dirfd, entry->d_name, &st, 0);
+        time_t modified = st.st_mtime;
+        if (modified >= max_time) {
+            printf("%s\n", entry->d_name);
+        }
+    }        
+    closedir(dir);
+}
---
+++ week8.fs_time/recursive_dirs.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+#include <limits.h>
+
+
+static void
+list_dir (const char * dir_name) {
+    DIR *d = opendir (dir_name);
+
+    if (!d) {
+        fprintf (stderr, "Cannot open directory '%s': %s\n",
+                 dir_name, strerror (errno));
+        exit (EXIT_FAILURE);
+    }
+    while (1) {
+        struct dirent *entry;
+        const char *d_name;
+
+        entry = readdir(d);
+        if (!entry) {
+            break;
+        }
+        d_name = entry->d_name;
+	    printf ("%s/%s\n", dir_name, d_name);
+
+
+        if (entry->d_type & DT_DIR) {
+            // Your code here
+	    }
+    }
+
+    if (closedir(d)) {
+        fprintf (stderr, "Could not close '%s': %s\n",
+                 dir_name, strerror (errno));
+        exit (EXIT_FAILURE);
+    }
+}
+
+int main () {
+    list_dir (".");
+    return 0;
+}
