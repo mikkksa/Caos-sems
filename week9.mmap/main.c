05878a65137006d51c12c26a200540ab6a0739ff
Mikhail Molchanov <m.mol4anov2010@yandex.ru>
2020-10-28 16:11:58 +0300

    my own malloc added

---
+++ week9.mmap/main.c
@@ -0,0 +1,42 @@
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stddef.h>
+#include <unistd.h>
+
+#include "my_malloc.h"
+
+void *mremap(void *old_address, size_t old_size,
+                    size_t new_size, int flags, ... /* void *new_address */);
+
+void *my_malloc(size_t size) {
+    void *mem = mmap(NULL, size + sizeof(size_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS |
+                    MAP_PRIVATE, -1, 0);
+    *((size_t*)(mem)) = size;
+
+    return (void*)(((size_t*)mem)+sizeof(size_t));
+}
+
+void free(void *ptr) {
+    size_t *size_ptr = ((size_t*)ptr)-sizeof(size_t);
+
+    munmap((void*)size_ptr, *size_ptr+sizeof(size_t));
+}
+
+void *realloc(void *ptr, size_t size) {
+    size_t *size_ptr = ((size_t*)ptr)-sizeof(size_t);
+    size_t old_size = *size_ptr;
+    void *new_ptr = mremap((void*)size_ptr, old_size+sizeof(size_t),
+                    size+sizeof(size_t), MREMAP_MAYMOVE, NULL);
+
+    *((size_t *)new_ptr) = size;
+    return (void*)((size_t *)new_ptr+sizeof(size_t));
+}
+
+
+int main() {
+    void *mem = my_malloc(1024);
+    printf("%p\n", mem);
+    free(mem);
+}
---
+++ week9.mmap/my_malloc.h
@@ -0,0 +1,3 @@
+void *my_malloc(size_t size);
+void free(void *ptr);
+void *realloc(void *ptr, size_t size);
